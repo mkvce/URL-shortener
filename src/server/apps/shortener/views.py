4fe9b784250c058f793966ff1f1e89e23030d19f
Mahdi <mahdi57.kamali1@gmail.com>
2021-09-17 00:00:51 +0430

    Add redirect view

--- src/server/apps/shortener/views.py
+++ src/server/apps/shortener/views.py
@@ -2,6 +2,8 @@ from .models import URL
 from rest_framework import generics
 from .serializers import UrlSerializer
 from ..utils.slug_generator import generate_slug
+from django.views.generic import RedirectView
+from rest_framework.generics import get_object_or_404
 
 
 # Create your views here.
@@ -13,3 +15,12 @@ class UrlView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(slug=generate_slug())
+
+
+class UrlRedirectView(RedirectView):
+    permanent = True
+
+    def get_redirect_url(self, slug):
+        url = get_object_or_404(URL, slug=slug)
+        url.increase_visits()
+        return url.address
