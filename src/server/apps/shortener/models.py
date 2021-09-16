9aac25132ea20096d694482cec1a013ce18edc52
Mahdi <mahdi57.kamali1@gmail.com>
2021-09-16 19:30:23 +0430

    Add model URL

--- src/server/apps/shortener/models.py
+++ src/server/apps/shortener/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class URL(models.Model):
+    address = models.URLField(max_length=256, verbose_name='آدرس لینک')
+    label = models.CharField(max_length=128, blank=True, verbose_name='برچسب')
+    slug = models.SlugField(unique=True, max_length=8)
+    visits = models.PositiveIntegerField(default=0, editable=False,
+                                         verbose_name="تعداد بازدیدها")
+    created = models.DateTimeField(auto_now_add=True, verbose_name='ایجاد شده در')
+
+    class Meta:
+        verbose_name = 'URL'
+
+    def __str__(self):
+        return self.address
+
+    def increase_visits(self):
+        self.visits += 1
+        super().save()
