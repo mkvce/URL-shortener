3ba960b899aaa87a9e531177aa75f6f9cf38e723
Mahdi <mahdi57.kamali1@gmail.com>
2021-09-16 21:14:49 +0430

    Add URL view

--- src/server/apps/shortener/views.py
+++ src/server/apps/shortener/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import URL
+from rest_framework import generics
+from .serializers import UrlSerializer
+from ..utils.slug_generator import generate_slug
+
 
 # Create your views here.
+
+
+class UrlView(generics.ListCreateAPIView):
+    queryset = URL.objects.all()
+    serializer_class = UrlSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(slug=generate_slug())
